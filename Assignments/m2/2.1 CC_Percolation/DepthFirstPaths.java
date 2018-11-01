4f41f2a53eab1d54bb66d7e9a73249d360c1db47
nemnous <nani.botta.123@msitprogram.net>
2018-11-01 10:16:19 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m2/2.1 CC_Percolation/DepthFirstPaths.java
+++ Assignments/m2/2.1 CC_Percolation/DepthFirstPaths.java
@@ -1,29 +1,46 @@
+/**
+ * Class for depth first paths.
+ */
 public class DepthFirstPaths {
-    private boolean[] marked;    // marked[v] = is there an s-v path?
-    private int[] edgeTo;        // edgeTo[v] = last edge on s-v path
-    private final int s;         // source vertex
-
     /**
-     * Computes a path between {@code s} and every other vertex in graph {@code G}.
-     * @param G the graph
-     * @param s the source vertex
+     * marked[v] - is there an s-v path.
+     */
+    private boolean[] marked;
+    /**
+     * edegeTo[v] - last edge on s-v path.
+     */
+    private int[] edgeTo;
+    /**
+     * source vertex.
+     */
+    private final int s;
+    /**
+     * Computes a path between {@code s}.
+     * and every other vertex in graph {@code graph}.
+     * @param graph the graph
+     * @param src the source vertex
      * @throws IllegalArgumentException unless {@code 0 <= s < V}
      */
-    public DepthFirstPaths(GraphClass G, int s) {
-        this.s = s;
-        edgeTo = new int[G.vertex()];
-        marked = new boolean[G.vertex()];
-        dfs(G, s);
+    public DepthFirstPaths(final GraphClass graph, final int src) {
+        this.s = src;
+        edgeTo = new int[graph.vertex()];
+        marked = new boolean[graph.vertex()];
+        dfs(graph, s);
     }
 
-    // depth first search from v
-    //The time complexity is O(N).
-    private void dfs(GraphClass G, int v) {
+    /**
+     * depth from search from v.
+     * The time complexity is O(N).
+     *
+     * @param      graph     { parameter_description }
+     * @param      v     { parameter_description }
+     */
+    private void dfs(final GraphClass graph, final int v) {
         marked[v] = true;
-        for (int w : G.adj[v]) {
+        for (int w : graph.adj[v]) {
             if (!marked[w]) {
                 edgeTo[w] = v;
-                dfs(G, w);
+                dfs(graph, w);
             }
         }
     }
@@ -35,10 +52,11 @@ public class DepthFirstPaths {
      * @return {@code true} if there is a path, {@code false} otherwise
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public boolean hasPathTo(int v) {
+    public boolean hasPathTo(final int v) {
         return marked[v];
     }
 
 
 
 }
+
