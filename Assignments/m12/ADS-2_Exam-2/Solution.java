a632fa7d8df0ba80c9aab509bdbafe393004aced
nemnous <nani.botta.123@msitprogram.net>
2018-11-10 11:33:28 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m12/ADS-2_Exam-2/Solution.java
+++ Assignments/m12/ADS-2_Exam-2/Solution.java
@@ -1,111 +1,122 @@
+/**
+ * Dijkstra algorithm uses time complexity of O(E log(V)).
+ */
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * unused constructor.
+         */
+    }
+    /**
+     * main function handles the input and returns the
+     * output to the console.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // Self loops are not allowed...
+        // Parallel Edges are allowed...
+        // Take the Graph input here...
+        Scanner scan = new Scanner(System.in);
 
-public class Solution {
+        int cities = scan.nextInt();
+        scan.nextLine();
+        int roadLines = scan.nextInt();
+        scan.nextLine();
+        EdgeWeightedGraph graph = new EdgeWeightedGraph(cities);
 
-	public static void main(String[] args) {
-		// Self loops are not allowed...
-		// Parallel Edges are allowed...
-		// Take the Graph input here...
-		Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < roadLines; i++) {
+            String[] paths = scan.nextLine().split(" ");
+            int cityA = Integer.parseInt(paths[0]);
+            int cityB = Integer.parseInt(paths[1]);
+            int distance = Integer.parseInt(paths[2]);
 
-		int cities = scan.nextInt();
-		scan.nextLine();
-		int roadLines = scan.nextInt();
-		scan.nextLine();
-		EdgeWeightedGraph graph = new EdgeWeightedGraph(cities);
+            graph.addEdge(new Edge(cityA, cityB, distance));
 
-		for(int i = 0; i < roadLines; i++) {
-			String[] paths = scan.nextLine().split(" ");
-			int cityA = Integer.parseInt(paths[0]);
-			int cityB = Integer.parseInt(paths[1]);
-			int distance = Integer.parseInt(paths[2]);
+        }
 
-			graph.addEdge(new Edge(cityA, cityB, distance));
+        String caseToGo = scan.nextLine();
 
-		}
+        switch (caseToGo) {
+        case "Graph":
+            //Print the Graph Object.
+            System.out.println(graph);
+            break;
 
-		String caseToGo = scan.nextLine();
+        case "DirectedPaths":
+            // Handle the case of DirectedPaths, where two integers are given.
+            // First is the source and second is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            String[] str = scan.nextLine().split(" ");
+            int s = Integer.parseInt(str[0]);
+            int d = Integer.parseInt(str[1]);
 
-		switch (caseToGo) {
-		case "Graph":
-			//Print the Graph Object.
-			System.out.println(graph);
-			break;
+            DijkstraSP dj = new DijkstraSP(graph, s);
 
-		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			String str[] = scan.nextLine().split(" ");
-			int s = Integer.parseInt(str[0]);
-			int d = Integer.parseInt(str[1]);
+            double dis = dj.distTo(d);
+            double inf = Double.POSITIVE_INFINITY;
+            if (dis == inf) {
+                System.out.println("No Path Found.");
+            } else {
+                System.out.println(dis);
+            }
+            break;
 
-			DijkstraSP dj = new DijkstraSP(graph, s);
+        case "ViaPaths":
+            String[] str1 = scan.nextLine().split(" ");
+            int src = Integer.parseInt(str1[0]);
+            int via = Integer.parseInt(str1[1]);
+            int des = Integer.parseInt(str1[2]);
 
-			double dis = dj.distTo(d);
-			double inf = Double.POSITIVE_INFINITY;
-			if(dis == inf) {
-				System.out.println("No Path Found.");
-			} else {
-				System.out.println(dis);
-			}
-			break;
+            DijkstraSP dj0 = new DijkstraSP(graph, src);
+            // System.out.println(dj.pathTo(via));
+            if (dj0.hasPathTo(via)) {
+                DijkstraSP dj1 = new DijkstraSP(graph, via);
 
-		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			
-			String str1[] = scan.nextLine().split(" "); 
-			int src = Integer.parseInt(str1[0]);
-			int via = Integer.parseInt(str1[1]);
-			int des = Integer.parseInt(str1[2]);
+                if (dj1.hasPathTo(des)) {
+                    System.out.println(dj0.distTo(via) + dj1.distTo(des));
+                    int temp = src;
+                    System.out.print(temp);
+                    int oth = 1;
+                    for (Edge i : dj0.pathTo(via)) {
+                        oth = temp;
+                        System.out.print(" " + i.other(oth));
+                        temp = i.other(oth);
+                    }
 
-			DijkstraSP dj0 = new DijkstraSP(graph, src);
-			// System.out.println(dj.pathTo(via));
-			if(dj0.hasPathTo(via)) {
-				DijkstraSP dj1 = new DijkstraSP(graph, via);
+                    while (oth != des) {
+                        for (Edge j : dj1.pathTo(des)) {
 
-				if(dj1.hasPathTo(des)) {
-					System.out.println(dj0.distTo(via) + dj1.distTo(des));
-					// System.out.println(dj0.pathTo(via) + " " + dj1.pathTo(des));
-					int temp = src;
-					System.out.print(temp);
-					int oth = 1;
-					for(Edge i : dj0.pathTo(via)) {
-						oth = temp;
-						System.out.print(" " + i.other(oth));
-						temp = i.other(oth);
-					}
+                            try {
+                                oth = temp;
+                                System.out.print(" " + j.other(oth));
+                                temp = j.other(oth);
+                            } catch (Exception e) {
+                                continue;
+                            }
 
-					while( oth != des) {
-						for(Edge j : dj1.pathTo(des)) {
-							
-							try {
-								oth = temp;
-								System.out.print(" " + j.other(oth));
-								temp = j.other(oth);
-							} catch(Exception e) {
-								continue;
-							}
+                        }
+                    }
+                } else {
+                    System.out.println("No Path Found.");
+                }
 
-						}
-					}
-				} else {
-				System.out.println("No Path Found.");					
-				}
+            } else {
+                System.out.println("No Path Found.");
+            }
+            break;
 
-			} else {
-				System.out.println("No Path Found.");
-			}
-			break;
+        default:
+            break;
+        }
 
-		default:
-			break;
-		}
-
-	}
+    }
 }
